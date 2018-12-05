1d4085b0b205a0d055cedf443db19dd5e7fef9d1
sonduong305 <sonduong305@gmail.com>
2018-12-05 14:29:31 +0700

    init

---
+++ src/beans/SearchBookResultBean.java
@@ -0,0 +1,76 @@
+package beans;
+
+public class SearchBookResultBean {
+    
+    private String bookId;
+    private String title;
+    private String authors = "Updating..";
+    private String branchId= "Updating..";
+    private String noOfCopies= "Updating..";
+    private String availability = "0";
+
+    @Override
+    public String toString() {
+        return "SearchBookResultBean{" + "bookId=" + bookId + ", title=" + title + ", authors=" + authors + ", branchId=" + branchId + ", noOfCopies=" + noOfCopies + ", availability=" + availability + '}';
+    }
+
+    public SearchBookResultBean(String bookId, String title, String authors, String branchId, String noOfCopies, String availability) {
+        this.bookId = bookId;
+        this.title = title;
+        this.authors = authors;
+        this.branchId = branchId;
+        this.noOfCopies = noOfCopies;
+        this.availability = availability;
+    }
+    public SearchBookResultBean(String bookId,String title) {
+    	this.bookId = bookId;
+        this.title = title;
+    }
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getNoOfCopies() {
+        return noOfCopies;
+    }
+
+    public void setNoOfCopies(String noOfCopies) {
+        this.noOfCopies = noOfCopies;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+}
---
+++ src/utilities/STATUS_TYPE.java
@@ -0,0 +1,5 @@
+package utilities;
+
+public enum STATUS_TYPE {
+    WARNING,ERROR,SUCCESS;
+}
